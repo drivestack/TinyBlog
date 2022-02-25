7ba1099b5b4ba8f3c1cd82bb338c5cfd8dfb3f46
yangh <yh130058@sjtu.edu.cn>
2022-02-25 19:43:06 +0800

    修改部分注释

--- src/main/java/com/shanqi/ssm/blog/interceptor/AdminInterceptor.java
+++ src/main/java/com/shanqi/ssm/blog/interceptor/AdminInterceptor.java
@@ -20,7 +20,9 @@ public class AdminInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws IOException {
-        //这里可以根据session的用户来判断角色的权限，根据权限来转发不同的页面
+        // 这里可以根据session的用户来判断角色的权限，根据权限来转发不同的页面
+        // 考虑使用 Redis 对这个进行改进，用户登录后，在 Redis 中存储用户信息的 Json 格式，key 为 Session 的 ID
+        // 设置其存活时间，例如5分钟，当用户出现任何操作时，更新其过期时间
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             response.sendRedirect("/login");
